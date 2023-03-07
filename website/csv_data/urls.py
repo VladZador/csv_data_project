4176fce6fb375bbdf8acbe1106832328307dfa1a
Vladyslav Zadorozhnii <vladzador.ua@gmail.com>
2023-03-07 19:31:34 +0200

    Add a valid post method for sending the data schema

--- website/csv_data/urls.py
+++ website/csv_data/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 
-from .views import UserFileView
+from .views import UserFileView, DataSchemaCreateView, DataSchemaRedirectView
 
 urlpatterns = [
-    path('', UserFileView.as_view(), name="user_files")
+    path('', UserFileView.as_view(), name="user_files"),
+    path('create-schema/',
+         DataSchemaCreateView.as_view(),
+         name="create_schema"
+         ),
+    path("create-schema/confirm/",
+         DataSchemaRedirectView.as_view(),
+         name="confirm_schema_creation"),
 ]
--- website/csv_data/views.py
+++ website/csv_data/views.py
@@ -1,7 +1,28 @@
-from django.views.generic import ListView
+import json
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import JsonResponse
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.generic import ListView, TemplateView
 
 from .models import UserFile
+from website.decorators import ajax_required
 
 
-class UserFileView(ListView):
+class UserFileView(LoginRequiredMixin, ListView):
     model = UserFile
+
+
+class DataSchemaCreateView(LoginRequiredMixin, TemplateView):
+    template_name = "csv_data/create_schema.html"
+
+
+@method_decorator(ajax_required, name="dispatch")
+class DataSchemaRedirectView(LoginRequiredMixin, View):
+
+    @staticmethod
+    def post(request, *args, **kwargs):
+        data_schema = json.load(request)["post_data"]
+
+        return JsonResponse(data={"redirectUrl": reverse_lazy("user_files")})
